61f7068dbc709547578b8f720c4c3504fdbbd550
Jaak Randmets <jaak@goblin.at.mt.ut.ee>
2008-10-31 16:07:10 +0200

    * Closures, code cleanup

--- src/CodeGen.hs
+++ src/CodeGen.hs
@@ -18,6 +18,7 @@ builtin BAdd = ADD
 builtin BMul = MUL
 builtin BSub = SUB
 builtin BLe  = LEQ
+builtin BEq  = EQUAL
 builtin _ = undefined
 
 codeGen :: AST -> [MaMa]
@@ -30,8 +31,8 @@ codev, codeb, codec  :: AST -> Cg ()
 -- simple expressions
 codeb (Lit x) = emit (LOADC x)
 codeb (Builtin bi es) = do
-  k <- asks sd
-  zipWithM_  (\sd' -> withSd sd' . codeb) [k ..] es
+  sd <- asks sd
+  zipWithM_  (\sd' -> withSd sd' . codeb) [sd ..] es
   emit (builtin bi)
 codeb (Ifte e t f) = do
   a <- newLabel
@@ -85,32 +86,37 @@ codev (Abs xs e) = do
     LABEL a,
     TARG k]
   withSd 0 $ withEnv env' $ codev e
-  emits [RETURN k, LABEL b]
+  emits [
+    RETURN k,
+    LABEL b]
 codev (App e es) = do
   a <- newLabel
-  emit (MARK a)
-  let m = length es
   sd <- asks sd
+  let m = length es
+  emit (MARK a)
   zipWithM_ (\sd' -> withSd sd' . codec) [sd + 3 ..] (reverse es)
-  withSd (sd + m + 3) (codev e)
-  emits [APPLY, LABEL a]
+  withSd (sd + m + 3) $ codev e
+  emits [
+    APPLY,
+    LABEL a]
 codev (Let False bs e) = do
   sd <- asks sd
   env <- asks env
   let
+    n = length bs
+    xs = [(y, e') | (y, [], e') <- bs]
     step env ((y, e'), i) = do
       withSd (sd + i) $ withEnv env $ codec e'
       return $ Env.insert y (Local (sd + i + 1)) env
   env' <- foldM step env (zip xs [0..])
   withSd (sd + n) $ withEnv env' $ codev e
   emit (SLIDE n)
-  where
-    n = length bs
-    xs = [(y, e') | (y, [], e') <- bs]
 codev (Let True bs e) = do
   sd <- asks sd
   env <- asks env
   let
+    n = length bs
+    (ys, es) = unzip [(y, e') | (y, [], e') <- bs]
     insert' = uncurry Env.insert
     env' = foldr insert' env $ zip ys (map Local [sd + 1..])
   emit (ALLOC n)
@@ -120,11 +126,27 @@ codev (Let True bs e) = do
       emit (REWRITE i)
     codev e
   emit (SLIDE n)
-  where
-    n = length bs
-    (ys, es) = unzip [(y, e') | (y, [], e') <- bs]
 
-codec = codev -- undefined
+-- closure stuff
+codec e = do
+  sd <- asks sd
+  env <- asks env
+  a <- newLabel
+  b <- newLabel
+  let
+    zs = fvs e
+    g = length zs
+    env' = Env.fromList $ zip zs (map Global [0..])
+  zipWithM_ (\sd' -> withSd sd' . getvar) [sd ..] zs
+  emits [
+    MKVEC g,
+    MKCLOS a,
+    JUMP b,
+    LABEL a]
+  withSd 0 $ withEnv env' $ codev e
+  emits [
+    UPDATE,
+    LABEL b]
 
 getvar :: Name -> Cg ()
 getvar x = do
@@ -160,21 +182,3 @@ fvs' (Let True bs e) = S.unions (fvs' e : ss) \\\ xs'
   where
     (xs', ss) = unzip [(x, fvs' e' \\\ xs) | (x, xs, e') <- bs]
 fvs' (Builtin _ es) = S.unions $ map fvs' es
-
---
-
-t 1 = Let False [
-    ("a", [], Lit 19),
-    ("b", [], Builtin BMul [Var "a", Var "a"])]
-  (Builtin BAdd [Var "a", Var "b"])
-
--- factorial of 8
-t 2 = Let True [
-    ("f", [], Abs ["x", "y"] (
-      Ifte (Builtin BLe [Var "y", Lit 1])
-        (Var "x")
-        (App (Var "f") [
-          Builtin BMul [Var "x", Var "y"],
-          Builtin BSub [Var "y", Lit 1]])
-    ))]
-  (App (Var "f") [Lit 1, Lit 8])
