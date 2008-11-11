490b551538645a1af0b52ac93e09236d94f0276e
Jaak Randmets <jaak.ra@gmail.com>
2008-11-11 19:42:44 +0200

    * code gen tests

--- src/testCodeGen.hs
+++ src/testCodeGen.hs
@@ -1,3 +1,8 @@
+{-# LANGUAGE OverloadedStrings, FlexibleInstances #-}
+
+import Control.Arrow (left, right)
+import Data.String
+
 import Pretty
 import CodeGen
 import AST
@@ -10,7 +15,8 @@ infix 0 .=
 infixl 1 `app`
 app = App
 
-instance Num AST where
+
+instance (Show a, Eq a) => Num (AST a) where
   fromInteger = Lit . fromInteger
   x + y = Builtin BAdd [x, y]
   x - y = Builtin BSub [x, y]
@@ -18,74 +24,75 @@ instance Num AST where
   abs = undefined
   signum = undefined
 
+instance IsString (AST String) where
+  fromString = Var
+
 --
 -- some tests:
 
 t 1 = Let NonRec [
     "a" .= 19,
-    "b" .= Var "a" * Var "a"]
-  (Var "a" + Var "b")
+    "b" .= "a" * "a"]
+  ("a" + "b")
 
 -- factorial of 8
 t 2 = Let Rec [
     "f" .= Abs ["x", "y"] (
       Ifte (Builtin BLe [Var "y", 1])
-        (Var "x")
-        (Var "f" `app` [
-          Var "x" * Var "y",
-          Var "y" - 1])
+        "x"
+        ("f" `app` ["x" * "y", "y" - 1])
     )]
-  (Var "f" `app` [1, 8])
+  ("f" `app` [1, 8])
 
 -- odd / even
 t 3 = Let Rec [
     "odd" .= (Abs ["x"] (
-        Ifte (Builtin BEq [Var "x", 0])
+        Ifte (Builtin BEq ["x", 0])
              0
-             (Var "even" `app` [Var "x" - 1])
+             ("even" `app` ["x" - 1])
       )),
     "even" .= (Abs ["x"] (
-        Ifte (Builtin BEq [Var "x", 0])
+        Ifte (Builtin BEq ["x", 0])
              1
-             (Var "odd" `app` [Var "x" - 1])
+             ("odd" `app` ["x" - 1])
       ))]
-  (Var "even" `app` [11])
+  ("even" `app` [11])
 
 --
 t 4 = Let NonRec [
-    "add" .= Abs ["x", "y"] (Var "x" + Var "y"),
-    "mul" .= Abs ["x", "y"] (Var "x" * Var "y")]
-  (Var "add" `app` [10, Var "mul" `app` [10, 11]])
+    "add" .= Abs ["x", "y"] ("x" + "y"),
+    "mul" .= Abs ["x", "y"] ("x" * "y")]
+  ("add" `app` [10, "mul" `app` [10, 11]])
 
 -- partial application
 t 5 = Let Rec [
-    "add" .= Abs ["x", "y"] (Var "x" + Var "y")]
-  (Var "add" `app` [10] `app` [11])
+    "add" .= Abs ["x", "y"] ("x" + "y")]
+  ("add" `app` [10] `app` [11])
 
 --
 t 6 = Let Rec [
-    "one" .= Abs ["x"] (App (Var "two") [Var "x"]),
-    "two" .= Abs ["x"] 2]
-  (Var "one" `app` [1])
+    "one" .= Abs ["x"] ("two" `app` ["x"]),
+    "two" .= Abs ["y"] 2]
+  ("one" `app` [1])
 
 -- infinite loop
 t 7 = Let Rec [
-    "a" .= Var "b",
-    "b" .= Var"a"]
-  (Var "a")
+    "a" .= "b",
+    "b" .= "a"]
+  "a"
 
 -- stupid Y combinator
 t 8 = Let Rec [
-    "fix" .= Abs ["f"] (Var "f" `app` [Var "fix" `app` [Var "f"]]),
-    "step" .= Abs ["f", "x"] (Ifte (Builtin BEq [Var "x", 1])
-      (Var "x")
-      (Builtin BMul [Var "x", Var "f" `app` [Builtin BSub [Var "x", 1]]]))]
-  ((Var "fix" `app` [Var "step"]) `app` [5])
+    "fix" .= Abs ["f"] ("f" `app` ["fix" `app` ["f"]]),
+    "step" .= Abs ["f", "x"] (Ifte (Builtin BEq ["x", 1])
+      "x"
+      ("x" * ("f" `app` ["x" - 1])))]
+  (("fix" `app` ["step"]) `app` [5])
 
--- ncier Y combinator
+-- nicer Y combinator
 t 9 = Let NonRec [
-    "fix" .= Abs ["f"] (Let Rec ["x" .= Var "f" `app` [Var "x"]] (Var "x")),
-    "step" .= Abs ["f", "x"] (Ifte (Builtin BEq [Var "x", 1])
-      (Var "x")
-      (Builtin BMul [Var "x", Var "f" `app` [Builtin BSub [Var "x", 1]]]))]
-  ((Var "fix" `app` [Var "step"]) `app` [5])
+    "fix" .= Abs ["f"] (Let Rec ["x" .= "f" `app` ["x"]] "x"),
+    "step" .= Abs ["f", "x"] (Ifte (Builtin BEq ["x", 1])
+      "x"
+      ("x" * ("f" `app` ["x" - 1])))]
+  (("fix" `app` ["step"]) `app` [5])
