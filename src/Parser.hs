f563a4b0d6bb5c23f044175930f7b8a6f7fdfa6c
Jaak Randmets <jaak@goblin.at.mt.ut.ee>
2008-11-12 15:44:01 +0200

    * Include dirs

--- src/Parser.hs
+++ src/Parser.hs
@@ -5,22 +5,30 @@ import qualified PufTokens as PT
 import qualified AST
 import qualified Data.ByteString.Lazy.Char8 as BS
 
+import Control.Monad (filterM)
+import System.Directory
+import System.FilePath
 
 rawTokens :: FilePath -> IO [PT.Token]
 rawTokens = fmap PT.alexScanTokens . BS.readFile
 
-expandIncludes :: [PT.Token] -> IO [PT.Token]
-expandIncludes [] = return []
-expandIncludes (PT.Include file : xs) = 
-    do rt <- rawTokens file 
-       ex <- expandIncludes rt 
-       rest <- expandIncludes xs
-       return $ ex ++ rest
-expandIncludes (x : xs) = fmap (x:) $ expandIncludes xs
+expandIncludes :: [FilePath] -> [PT.Token] -> IO [PT.Token]
+expandIncludes includes = loop
+  where
+    loop [] = return []
+    loop (PT.Include file : xs) =  do
+      fs <- filterM doesFileExist $ map (</> file) includes
+      case fs of
+        [] -> fail "Include not found"
+        (f : _) -> do
+          rt <- rawTokens f
+          ex <- loop rt
+          rest <- loop xs
+          return $ ex ++ rest
+    loop (x : xs) = fmap (x:) $ loop xs
 
-parseFile :: FilePath -> IO [AST.Binding String]
-parseFile fp = fmap PP.parseFile $ rawTokens fp >>= expandIncludes
-       
-            
+parseFile :: [FilePath] -> FilePath -> IO [AST.Binding String]
+parseFile includes fp = fmap PP.parseFile $ rawTokens fp >>= expandIncludes includes
+
 parsePuf  :: [PT.Token] -> Either String (AST.AST String)
 parsePuf code = Right $ PP.parseExpr code
