bbcfbcb7a967545ee2f1df2cf25a3f7138f82ad5
Jaak Randmets <jaak.ra@gmail.com>
2008-11-23 17:08:11 +0200

    * make Main.hs nicer

--- src/Main.hs
+++ src/Main.hs
@@ -20,8 +20,6 @@ import qualified OptMaMa
 
 data Options = Options {
     printHelp :: Bool,
-    eval :: Bool,
-    mama :: Bool,
     debug :: Bool,
     optMaMa :: Bool,
     includePaths :: [FilePath]
@@ -31,8 +29,6 @@ data Options = Options {
 defaultOpts :: Options
 defaultOpts = Options {
     printHelp = False,
-    eval = False,
-    mama = True,
     debug = False,
     optMaMa = False,
     includePaths = ["."]
@@ -40,26 +36,35 @@ defaultOpts = Options {
 
 optsAddInclude :: FilePath -> Options -> Options
 optsAddInclude fpath o = o { includePaths = fpath : includePaths o }
-
 optsEnableHelp o = o { printHelp = True }
-
-optsEnableEval o = o { eval = True, mama = False }
-
-optsEnableMama o = o { mama = True, eval = False }
-
 optsEnableDebug o = o { debug = True }
-
 optsEnableOptMaMa o = o { optMaMa = True }
 
 options :: [OptDescr (Options -> Options)]
 options = [
-    Option ['h'] ["help"] (NoArg $ optsEnableHelp) "Display the help message",
-    Option ['I'] ["include"] (ReqArg optsAddInclude "DIR") "Include paths",
-    Option ['e'] ["evaluate"] (NoArg $ optsEnableEval) "Evaluate given file",
-    Option ['g'] ["debug"] (NoArg $ optsEnableDebug) "Output some debug info",
-    Option ['O'] ["opt-mama"] (NoArg $ optsEnableOptMaMa) "Optimise generated mama code",
-    Option [] ["mama"] (NoArg $ optsEnableMama) "Output MaMa bytecode"
+    Option ['h'] ["help"]      (NoArg $ optsEnableHelp)      "Display the help message",
+    Option ['I'] ["include"]   (ReqArg optsAddInclude        "DIR") "Include path",
+    Option ['g'] ["debug"]     (NoArg $ optsEnableDebug)     "Output some debug info",
+    Option ['O'] ["opt-mama"]  (NoArg $ optsEnableOptMaMa)   "Optimise generated mama code"
   ]
+
+handleOpt :: [FilePath] -> Options -> String -> IO ()
+handleOpt files opt usage | printHelp opt = putStrLn usage
+handleOpt files opt _ = forM_ files $ \file -> do
+  when (debug opt) $ do
+    putStrLn $ "== " ++ file ++ " =="
+  bs <- parseFile (includePaths opt) file
+  sup <- Unique.newSupply
+  case Rename.rename sup $ AST.LetRec bs (AST.Var "main") of
+    Left err -> putStr $ "Errur: " ++ show err
+    Right t -> do
+      let t' = LastCall.detect (depAnal t)
+      when (debug opt) $ do
+        putStrLn "\n== Abstract syntax tree =="
+        putStrLn $ prettyAST $ t'
+        putStrLn "== / ==\n"
+      let f = if optMaMa opt then OptMaMa.optimise else id
+      mapM_ print $ f $ codeGen sup t'
 
 main :: IO ()
 main = do
@@ -68,22 +73,7 @@ main = do
   case getOpt Permute options args of
     (opts, files, []) -> do
       let opt = foldr ($) defaultOpts opts
-      if printHelp opt
-        then putStr $ usageInfo "Help! I'm trapped in the IO monad!" options
-        else forM_ files $ \file -> do
-          when (debug opt) $ do
-            putStrLn $ "== " ++ file ++ " =="
-          bs <- parseFile (includePaths opt) file
-          sup <- Unique.newSupply
-          case Rename.rename sup $ AST.LetRec bs (AST.Var "main") of
-            Left err -> putStr $ "Errur: " ++ show err
-            Right t -> do
-              let t' = LastCall.detect (depAnal t)
-              when (debug opt) $ do
-                putStrLn "\n== Abstract syntax tree =="
-                putStrLn $ prettyAST $ t'
-                putStrLn "== / ==\n"
-              when (mama opt) $ do
-                let f = if optMaMa opt then OptMaMa.optimise else id
-                mapM_ print $ f $ codeGen sup t'
-              when (eval opt) $ return ()
+      handleOpt
+        files
+        opt
+        (usageInfo "./tm [OPTION] [FILE]" options)
